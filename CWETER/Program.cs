4e7ac8e7f91797b7f7162c05c788fc2d63baca3f
4Mgr1 <4Mgr1@415-15>
2025-11-20 10:01:48 +0100

    Dodaj pliki projektów.

---
+++ CWETER/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace gibiocena
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {//67
+            int ocena;
+            Console.WriteLine("Podaj swoją ocenę a my powiemy Ci jak ją napisać nie cyfrą lecz pisemnie:");
+            string chwilowe = Console.ReadLine();
+            int.TryParse(chwilowe, out ocena);
+            switch (ocena)
+            {
+                case 1:
+                    Console.WriteLine($"Twoja ocena: {ocena} - niedostateczny");
+                    break;
+                case 2:
+                    Console.WriteLine($"Twoja ocena: {ocena} - dopuszczający");
+                    break;
+                case 3:
+                    Console.WriteLine($"Twoja ocena: {ocena} - dostateczny");
+                    break;
+                case 4:
+                    Console.WriteLine($"Twoja ocena: {ocena} - dobry");
+                    break;
+                case 5:
+                    Console.WriteLine($"Twoja ocena: {ocena} - bardzo dobry");
+                    break;
+                case 6:
+                    Console.WriteLine($"Twoja ocena: {ocena} - celujący");
+                    break;
+                default:
+                    Console.WriteLine("podaj ocenę od 1 do 6");
+                    break;
+            }
+
+        }
+    }
+}
